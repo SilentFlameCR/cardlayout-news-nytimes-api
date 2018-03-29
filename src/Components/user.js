27548f8d24c0a7ab460997959491cb8b53ac546f
SilentFlameCR <rodr0180@algonquinlive.com>
2018-03-28 22:33:08 -0400

    Fixed the previous fix that skewed user imgs

--- src/Components/user.js
+++ src/Components/user.js
@@ -8,11 +8,15 @@ class User extends React.Component {
     render() {
         return(
             <div className="user-div col-xs-7">
-                <div className="user-img col-xs-2">
-                  <img src={this.props.image} alt={this.props.first} className="userImage" />  
-                </div>
-                
-                <div className="user-name col-xs-8">{this.props.first + " " + this.props.last}<br></br> {this.state.read} mins ago </div>
+               <div className="row">
+                    <div className="user-img col-xs-2">
+                      <img src={this.props.image} alt={this.props.first} className="userImage" />  
+                    </div>
+
+                    <div className="user-name col-xs-8">
+                        {this.props.first + " " + this.props.last}<br></br> {this.state.read} mins ago 
+                    </div>
+               </div>
             </div>
         );
     }
